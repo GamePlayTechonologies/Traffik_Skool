3c3f55a4ea48c0de8291a456f76040fd40f41fdc
GamePlayTechonologies <mustang.e16@gmail.com>
2023-04-11 11:53:30 -0700

    202304011 Version 0.1

--- core/src/com/gameplaytechnologies/game/GameRenderer.java
+++ core/src/com/gameplaytechnologies/game/GameRenderer.java
@@ -1,31 +1,18 @@
+package com.gameplaytechnologies.game;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.gameplaytechnologies.game.GameWorld;
-import com.gameplaytechnologies.game.PlayerCar;
-
 
 public class GameRenderer {
-    private GameWorld gameWorld;
     private SpriteBatch spriteBatch;
+    private GameWorld gameWorld;
     private Texture background;
 
     public GameRenderer(GameWorld gameWorld) {
         this.gameWorld = gameWorld;
         spriteBatch = new SpriteBatch();
-        background = new Texture("background.png");
-    }
-
-    }
-    public void render() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        spriteBatch.begin();
-        spriteBatch.draw(background, 0, 0, gameWorld.getScreenWidth(), gameWorld.getScreenHeight());
-        PlayerCar playerCar = gameWorld.getPlayerCar();
-        playerCar.draw(spriteBatch);
-        spriteBatch.end();
+        background = new Texture(Gdx.files.internal("background.png"));
     }
 }
